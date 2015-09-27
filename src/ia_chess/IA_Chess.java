92b1785f4fe0748574fdebfa20dd4d98652b613a
Yuli <Yuli@PONEI>
2015-09-26 23:33:50 -0500

    Merge origin/master
    Conflicts:
    	src/ia_chess/IA_Chess.java

--- src/ia_chess/IA_Chess.java
+++ src/ia_chess/IA_Chess.java
@@ -6,13 +6,28 @@ public class IA_Chess {
 
     public static void main(String[] args)
     {
-        /*
         Board a = new Board();
         Chess ajedrez = new Chess();
-        ajedrez.start();
-                */
+        ajedrez.generarMovimiento();
+        ajedrez.realizarMovimiento(new Position(1,1),new Position(1,1));
+
+        Scanner teclado = new Scanner(System.in);
+        String xyTo;
+        String xyFrom;
+        while(true)
+        {
+            ajedrez.print();
+            System.out.println("Ingrese de ficha a mover: ");
+            xyTo = teclado.next();
+            System.out.println("Ingrese coordenadas de nueva posicion: ");
+            xyFrom = teclado.next();
+            ajedrez.realizarMovimiento(new Position(1,1),new Position(1,1));
+            ajedrez.generarMovimiento();
+        }       
+        /*
         Board b = new Board();
         Evaluacion e = new Evaluacion();
         System.out.println(e.evaluar(b, false));
+        */
     }
 }
